5171a010b6787fc8259394298b9b9b45d08f9b7e
Bhaskar Bansal <bhaskarbansal@Bhaskars-MacBook-Air.local>
2025-05-27 12:08:34 +0530

    my changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://mailerfront.netlify.app',
   methods: ['GET', 'POST'],
   credentials: true
 }));
